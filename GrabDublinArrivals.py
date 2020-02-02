61221add9082b1257dbe596667a5f368306559f3
Fergal McGovern <fergal.mcgovern@origamienergy.com>
2020-02-02 00:48:28 +0000

    WIP Update for new website format. Convert Script into functions.

--- GrabDublinArrivals.py
+++ GrabDublinArrivals.py
@@ -1,48 +1,64 @@
-#GRABDUBLINARRIVALS.py takes arrivals data from dublin airport website and Saves
-#it in json format to my working directory.
-
-#----------------------------------Setup
-import requests
+import ast
 from bs4 import BeautifulSoup
 import json
+import re
+import requests
 
-url_to_scrape = 'https://www.dublinairport.com/flight-information/live-departures'
+#----------------------------------About:
+#Takes arrivals data from dublin airport website and Saves
+#it in json format to my working directory.
 
-#----------------------------------Prepare Data
-r = requests.get(url_to_scrape)
+def getHtmlSoup(url_to_scrape):
+    r = requests.get(url_to_scrape)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup
 
-soup = BeautifulSoup(r.text, 'html.parser')
-##print(soup.prettify())
+def extractPayload(html_str):
+    #Regular Expression reminder:
+    #"." matches any character except a newline.
+    #"*" to match 0 or more repetitions of the preceding RE
+    regex = re.compile(r'(var jsonFlightList =)(\[.*\])')
+    firstMatch = regex.findall(html_str)[0]
+    payloadStr = firstMatch[1]
+    payloadList = json.loads(payloadStr)
+    return payloadList
 
-#----------------------------------
-table_data = [] #This is a list
+def getFlightList(url_to_scrape):
+    soup = getHtmlSoup(url_to_scrape)
+    html_str = str(soup)
+    payload = extractPayload(html_str)
+    return(payload)
 
-table_body = soup.tbody.find_all('tr') #<tbody> is a selector/tag
-#<tr> is also a selector/tag
+def saveListAsJson(myList, fileName):
+    with open(fileName, 'w') as f:
+        json.dump(myList, f)
+        #f.write(myList)
 
-for table_row in table_body: #For each thing in the table_body
-    flight_data = {} #This is a dictionary
+if __name__ == "__main__":
+    url_to_scrape = 'https://www.dublinairport.com/flight-information/live-departures'
 
-    flight_data['Origin'] = table_row.find_all('td')[1].text.strip() #The 1 here picks out Origin as the 1+1th data element in the row
-    flight_data['Airline'] = table_row.find_all('td')[2].text.strip() #The 1 here picks out Origin as the 1+1th data element in the row
+    flightList = getFlightList(url_to_scrape)
 
-    table_data.append(flight_data)
+    saveListAsJson(flightList, 'DublinArrivals.json')
 
-#print(table_data[:10])#Print the things with <td> tag
-    #The things we print here are ~ the data values from each row.
+#-------------------------------WIP
+#Conform to Naming Conventions for variables, functions...
+#Make this test driven development
+#Use Below functions for ideas
+def filterFlightID(flightList, flightId):
+    """Filters for a given flightID""" 
 
-for flight in table_data[:10]:
-    Origin = flight['Origin']
-    print(Origin)
+def selectDateRange(flightList, startDate, endDate):
+    """Selects trades between start (inclusive of) and end date (exclusive of)."""
+    #Can make endDate an optional argument. If not provided startDate = endDate.
+    return None
 
-for flight in table_data[:10]:
-    Origin = flight['Airline']
-    print(Origin)
+def printOrigins(table_data):
+    for flight in table_data[:10]:
+        Origin = flight['Origin']
+        print(Origin)
 
-#Recap of the above
-#   Each row from the website's table is a dictionary entry
-#   table_data is a list of all the dictionary entries
-
-#Save out as Json
-with open('DublinArrivals_2018-12-23.json', 'w') as fout:
-    json.dump(table_data,fout)
+def printAirlines(table_data):
+    for flight in table_data[:10]:
+        Origin = flight['Airline']
+        print(Origin)
