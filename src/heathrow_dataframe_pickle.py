d1c559e0b7c0ad2518c3bc4390e20e7c984d6ec2
FergM <fergalmacshamhrain@gmail.com>
2020-02-23 19:48:43 +0000

    woop script that combines all the parsed data to one dataframe

--- src/heathrow_dataframe_pickle.py
+++ src/heathrow_dataframe_pickle.py
@@ -1,59 +1,45 @@
-"""
-1) initialise dataframe
-
-2) Create Append everything
-for day in dataFiles:
-    makeDayDataframe
-    dataframe.append() it
-
-3) save dataframe as pickle
-"""
-#----------------------------------initialise
-import pandas as pd
-
-mydf = pd.DataFrame()
-print("\nempty df is:", mydf)
-#----------------------------------Append Everything
-#2.2) need to add a for loop
 from os import listdir
+import pandas as pd
+import pickle
 
-filenames = [item for item in listdir("../data/heathrow_data/") if item[-6:]=="Z.json"]
-print("Filenames are:\n\t", filenames)
-
-#2.1) simplify heathrow parsing and use it to create dataframe
-    #Select File
 from heathrow_extraction import load_data, extract_flight_list
 from heathrow_parsing import ParsedFlights
 
-full_file_name = "../data/heathrow_data/2020-02-02Z.json"
+heathrow_df = pd.DataFrame()
+print("\nempty df is:", heathrow_df)
 
-#Extract Raw Data
-raw_data = load_data(full_file_name)
-raw_flights = extract_flight_list(raw_data)
+#----------------------------------Append Everything
 
-#Parse Flights
-parsed_flights = ParsedFlights(raw_flights)
-flights_df = parsed_flights.to_dataframe()
+dir_path = "../data/heathrow_data/"
+filenames = [item for item in listdir(dir_path) if item[-6:]=="Z.json"]
+filenames.sort() # To avoid ambiguity.
+print("Filenames are:\n\t", filenames)
 
-print("\nempty df is:", mydf)
-mydf = mydf.append(flights_df)
-print("\nempty df.head() is NOW:\n", mydf.head(), "\n\n")
+for file in filenames:
+    path = dir_path + file
+
+    #Extract Raw Data
+    raw_data = load_data(path)
+    raw_flights = extract_flight_list(raw_data)
+    #Parse Flights
+    parsed_flights = ParsedFlights(raw_flights)
+    flights_df = parsed_flights.to_dataframe()
+    #Append
+    heathrow_df = heathrow_df.append(flights_df)
+    print("Last line is:\n", heathrow_df.tail(1))
 
 #----------------------------------Pickle
-import pickle
+print("\nHead is:\n", heathrow_df.head())
+print("Tail is:\n", heathrow_df.tail())
 
-var1 = ["This is a hello world string."]
-var2 = ["empty"]
-file_name = "myPickle.p"
-
-print("Var1:", var1)
-print("Var2:", var2)
+file_name = "heathrow_draft.p"
 
 with open( file_name, "wb" ) as f:
-    pickle.dump( var1, f)
+    pickle.dump( heathrow_df, f)
 
 with open(file_name, "rb") as f2:
-    var2 = pickle.load(f2)
+    loaded_df = pickle.load(f2)
 
-print("Var1:", var1)
-print("Var2:", var2)
+print("\nLoaded Head is:\n", loaded_df.head())
+print("Loaded Tail is:\n", loaded_df.tail())
+print("\n\n")
