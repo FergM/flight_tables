6802fef530e20b18905bf990ab4ae4fc50a93d7f
FergM <fergalmacshamhrain@gmail.com>
2020-04-05 09:31:51 +0100

    Untangle generic flight parsing from heathrow parsing

--- flight_tables/heathrow_parsing.py
+++ flight_tables/heathrow_parsing.py
@@ -1,15 +1,54 @@
+from bs4 import BeautifulSoup
 from datetime import datetime
+import json
+import requests
+import sys
 
-def extract_batch_heathrow(response):
-    batch_info = [] # List with `flight_info` from multiple flights
+def fetch_heathrow_data(iso_date_str, direction):
+    """
+    Returns python object with data from Heathrow API.
+    Inputs: 
+        `iso_date_str` (str): YYYY-MM-DD format of the date for which you want data.
+        `direction` (str): Specify whether you want "departures" or "arrivals"
+    Outputs: 
+        `heathrow_data` (dict);
+            Successful = 
+                * Python Dictionary with keys: ['header', 'flightSummaryList', 'references']
+                * Payload is the list output['flightSummaryList']['flight']
+                * Where each dictionary in the list represents a different flightID.
+            Failure = 
+                * Wrong Date will sys.exit("message")
+    """
+    def check_date_format(iso_date_str):
+        """Exit if date is not an iso date string"""
+        try:
+            datetime.strptime(iso_date_str, '%Y-%m-%d')
+        except ValueError as err:
+            sys.exit(f"Invalid Date: {iso_date_str}. Required Format is YYYY-MM-DD")
+        return iso_date_str
 
-    raw_flights = response['flightSummaryList']['flight']   
+    def make_url(iso_date_str, direction):
+        file_path = ""#./data/heathrow_data/"
+        file_name = iso_date_str + 'Z'
+        full_file_name = file_path + file_name + ".json"
+        url_root = 'https://api-dp-prod.dp.heathrow.com/infohub/api/v1/flights/' + direction + '/inactive/'
+        url = url_root + file_name
+        return url
 
-    for flight in raw_flights:
-        flight_info = extract_flight_heathrow(flight)
-        batch_info.append(flight_info) 
+    def fetch_html_soup(url_to_scrape):
+        r = requests.get(url_to_scrape)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        return soup
 
-    return batch_info
+    def soup_to_dict(soup, iso_date_str):
+        return json.loads(str(soup))
+
+    check_date_format(iso_date_str)
+    url = make_url(iso_date_str, direction)
+    soup = fetch_html_soup(url)
+    heathrow_data = json.loads(str(soup))
+
+    return heathrow_data
 
 def extract_flight_heathrow(raw_flight):
     #Input:
@@ -63,3 +102,14 @@ def extract_flight_heathrow(raw_flight):
     flight_info["code_share_type"] = raw_flight["codeShareType"]
 
     return flight_info
+
+def extract_batch_heathrow(response):
+    batch_info = [] # List with `flight_info` from multiple flights
+
+    raw_flights = response['flightSummaryList']['flight']   
+
+    for flight in raw_flights:
+        flight_info = extract_flight_heathrow(flight)
+        batch_info.append(flight_info) 
+
+    return batch_info
