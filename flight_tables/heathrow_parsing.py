a0440915da55aaf456513229fa15729f32c9626b
FergM <fergalmacshamhrain@gmail.com>
2020-07-08 16:53:43 +0100

    Draft parsing solution for new pihub endpoint. Testing needed. Overwrites the infohub endpoint parser, plan to keep both though.

--- flight_tables/heathrow_parsing.py
+++ flight_tables/heathrow_parsing.py
@@ -28,11 +28,19 @@ def fetch_heathrow_data(iso_date_str, direction):
         return iso_date_str
 
     def make_url(iso_date_str, direction):
-        url = 'https://api-dp-prod.dp.heathrow.com/infohub/api/v1/flights/' + direction + '/inactive/' + iso_date_str + 'Z'
+
+        # String Mapping
+        ## Used to get the required url string
+        direction_order_by = {'arrivals':'localArrivalTime', 'departures': 'localDepartureTime'}
+
+        # Flight Data Request pattern
+        url = f'https://api-dp-prod.dp.heathrow.com/pihub/flights/{direction}?date={iso_date_str}&orderBy={direction_order_by[direction]}'
+
         return url
 
     def fetch_html_soup(url_to_scrape):
-        r = requests.get(url_to_scrape)
+        headers={"origin": "https://www.heathrow.com"}
+        r = requests.get(url_to_scrape, headers = headers)
         soup = BeautifulSoup(r.text, 'html.parser')
         return soup
 
@@ -46,12 +54,14 @@ def fetch_heathrow_data(iso_date_str, direction):
 
     return heathrow_data
 
-def extract_flight_heathrow(raw_flight):
-    #Input:
-    #   raw_flight (dict): Dictionary for a single flight, pulled from the raw response.
-    #Output:
-    #   flight_info (dict): Dictionary with key information for the flight.
+def extract_flight_heathrow_pihub(raw_flight):
+    """Parse a raw flight sourced from the pihub/ endpoint
 
+    Input:
+       raw_flight (dict): Dictionary for a single flight, pulled from the raw response.
+    Output:
+       flight_info (dict): Dictionary with key information for the flight.
+    """
     assert type(raw_flight) == dict
     #Initialise empty dataframe
     flight_info = {'flight_id': None,
@@ -62,33 +72,36 @@ def extract_flight_heathrow(raw_flight):
                    'actual_datetime': None,
                    'code_share_type': None}
 
-    flight_info["flight_id"] = raw_flight['flightIdentifier']
+    flight_info["flight_id"] = raw_flight['flightService']['iataFlightIdentifier']
 
     # Get Origin and Destination
-    flight_info["origin"] = raw_flight['origin']['airportIataRef']
-    flight_info["destination"] = raw_flight['destination']['airportIataRef']
+    flight_info["origin"] = raw_flight['flightService']['aircraftMovement']['route']['portsOfCall'][0]['airportFacility']['iataIdentifier']
+    flight_info["destination"] = raw_flight['flightService']['aircraftMovement']['route']['portsOfCall'][1]['airportFacility']['iataIdentifier']
 
-    # Get Status and Location
-    try:
-        status = raw_flight['origin']['status']['messages']['message'][0]['text']
-        location = 'origin'
-    except:
-        status = raw_flight['destination']['status']['messages']['message'][0]['text']
-        location = 'destination'
-
+    # Get Status
+    flight_info["status"] = raw_flight['flightService']['aircraftMovement']['aircraftMovementStatus'][0]['statusData'][0]['localisationKey']
+    
     # Get Scheduled Time
-    scheduled_time_str = raw_flight[location]['scheduledDateTime']['local']
-    flight_info["scheduled_datetime"] = datetime.strptime(scheduled_time_str[:-7], "%Y-%m-%dT%H:%M")
-
-    # Assign Status
-    flight_info["status"] = status
+    ## Get Direction (Arrival or Departure)
+    direction_char = raw_flight['flightService']['arrivalOrDeparture']
+    direction_char_dict = {'A':'Arrival','D':'Departure'}
+    direction = direction_char_dict[direction_char]    
+    ## Get Scheduled Time (Local BST Timezone)
+    ### Assuming ORIGIN portsOfCall always comes first. 
+    ##### For departures we need to look at the time associated with the origin.
+    ##### For arrivals we need to look at the time associated with the destination.
+    direction_bool_dict = {'Arrival':1,'Departure':0}
+    direction_bool = direction_bool_dict[direction]    
+    scheduled_time_str = raw_flight['flightService']['aircraftMovement']['route']['portsOfCall'][direction_bool]['operatingTimes']['scheduled']['local']
+    flight_info["scheduled_datetime"] = datetime.strptime(scheduled_time_str, "%Y-%m-%dT%H:%M:%S")
 
     # Assign Actual Time
+    status = flight_info['status']
     if status == "Cancelled":
-        flight_info["status"] = "Cancelled"
+        pass
     elif (status == "Departed") or (status=="Landed"):
-        actual_date_str = raw_flight[location]['status']['statusTime'][0:11]
-        actual_time_str = raw_flight[location]['status']['messages']['message'][0]['data']
+        actual_time_str = raw_flight['flightService']['aircraftMovement']['aircraftMovementStatus'][0]['statusData'][0]['data']        
+        actual_date_str = scheduled_time_str[0:11]
         actual_datetime_str = actual_date_str + actual_time_str
         flight_info["actual_datetime"] = datetime.strptime(actual_datetime_str, "%Y-%m-%dT%H:%M")
     else:
@@ -104,7 +117,7 @@ def extract_flight_heathrow(raw_flight):
 
         return new_name
 
-    raw_codeshare_name = raw_flight["codeShareType"]
+    raw_codeshare_name = raw_flight['flightService']['codeShareStatus']
     flight_info["code_share_type"] = standardise_codeshare_name(raw_codeshare_name)
 
     return flight_info
@@ -112,10 +125,10 @@ def extract_flight_heathrow(raw_flight):
 def extract_batch_heathrow(response):
     batch_info = [] # List with `flight_info` from multiple flights
 
-    raw_flights = response['flightSummaryList']['flight']   
+    raw_flights = response
 
     for flight in raw_flights:
-        flight_info = extract_flight_heathrow(flight)
+        flight_info = extract_flight_heathrow_pihub(flight)
         batch_info.append(flight_info) 
 
     return batch_info
