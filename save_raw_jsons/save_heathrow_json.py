4b839bae93c8f1eae1aeb5f764e0505ce893c943
FergM <fergalmacshamhrain@gmail.com>
2020-07-09 11:17:16 +0100

    Update json save script.

--- save_raw_jsons/save_heathrow_json.py
+++ save_raw_jsons/save_heathrow_json.py
@@ -1,42 +1,45 @@
-from bs4 import BeautifulSoup
 import datetime
 import json
-import requests
 import sys
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
 
 def saveJson(soup, fileName):
     json_data = json.loads(str(soup))
     with open(fileName, 'w') as f:
         json.dump(json_data, f)
-        #f.write(myList)
 
 def saveHtml(soup, fileName):
 	with open(fileName, "w", encoding='utf-8') as file:
     		file.write(str(soup))
 
+def manualConfirmDates(first_date, last_date):
+    """Ask User to Confirm request Dates
+    
+    Break and exit if not confirmed.
+    """
+    print("First date will be: ", first_date.isoformat())
+    print("Last date will be: ", last_date.isoformat())
+    print(f"Direction = {direction}")
+    choice = input("Type y to continue:")
+    if choice != "y":
+        sys.exit("\nEXIT user did not want to continue.\n")
+
 # String Mapping
-## Used to get the required url string
+## Used to determine the pihub endpoint url
 direction_order_by = {'arrivals':'localArrivalTime', 'departures': 'localDepartureTime'}
 
 # Request Headers 
-## Copied from Chrome DevTools XHR request
-## Optional Cookie is removed
-request_headers = {
-"accept": "application/json, text/plain, */*",
-"accept-encoding": "gzip, deflate, br",
-"accept-language": "en-GB,en-US;q=0.9,en;q=0.8,fr;q=0.7,ga;q=0.6",
-"origin": "https://www.heathrow.com",
-"sec-fetch-dest": "empty",
-"sec-fetch-mode": "cors",
-"sec-fetch-site": "same-site",
-"user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-}
+request_headers = {"origin": "https://www.heathrow.com",}
 
 if __name__ == "__main__":
     """Ping heathrow endpoint and save arrivals/departures as json.
     
     Parameters:
         sys.argv[1] (str): Flight direction, either 'arrivals' or 'departures'.
+        sys.argv[2] (str): API endpoint name, either 'infohub' or 'pihub'.
     Returns:
         Success:
             .json (file): Json file with flight data. Saved to working directory.
@@ -46,34 +49,39 @@ if __name__ == "__main__":
     # Set Flight Direction
     try:
         direction = sys.argv[1]
+        assert (direction == "arrivals") or (direction == "departures"), f"Direction parameter {direction} is not valid."
     except:
         print("Direction parameter not specified. Assume `direction = arrivals`")
         direction = "arrivals"
 
-    #Select Date Range
-    last_date = datetime.date.today()-datetime.timedelta(days=1) #Or datetime(2020, 2,24).date()
-    first_date =  last_date-datetime.timedelta(days=4) 
+    # Determine Endpoint
+    try:
+        endpoint = sys.argv[2]
+        assert (endpoint == "infohub") or (endpoint == "pihub"), f"endpoint parameter {endpoint} is not valid."
+    except:
+        raise Exception("Error. API endpoint parameter not specified.\n")
 
-    delta = last_date - first_date
-    number_of_days = delta.days + 1
+    #Select Dates
+    number_of_days = 4
+    last_date = datetime.date.today()-datetime.timedelta(days=1) # Yesterday
+    dates = pd.date_range(end=last_date, periods=number_of_days).date
+    ## confirm dates
+    manualConfirmDates(dates[0], dates[-1])
 
-    #Ask User to Confirm
-    print("First date will be: ", first_date.isoformat())
-    print("Last date will be: ", last_date.isoformat())
-    print(f"Direction = {direction}")
-    choice = input("Type y to continue:")
-    if choice != "y":
-        sys.exit("\nEXIT user did not want to continue.\n")
+    # Save Response for Each Date
+    for date in dates:
 
-    # Loop: Request & Save for Each Date
-    date = first_date
-    for i in range(0, number_of_days):
         # Date string
         iso_date_str = date.isoformat()
 
-        # Request
+        # Send Request
         ## Prepare Request URL
-        url = f'https://api-dp-prod.dp.heathrow.com/pihub/flights/{direction}?date={iso_date_str}&orderBy={direction_order_by[direction]}'
+        if endpoint == "infohub":
+            url = f'https://api-dp-prod.dp.heathrow.com/infohub/api/v1/flights/{direction}/inactive/{iso_date_str}Z'
+        elif endpoint == "pihub":
+            url = f'https://api-dp-prod.dp.heathrow.com/pihub/flights/{direction}?date={iso_date_str}&orderBy={direction_order_by[direction]}'
+        else:
+            print(f"Endpoing name {endpoint} with status code {r.status_code}.")
         ## Send Request
         r = requests.get(url, headers = request_headers)
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -87,6 +95,3 @@ if __name__ == "__main__":
             print("File saved for", iso_date_str)
         else:
             print(f"Failed for {iso_date_str} with status code {r.status_code}.")
-
-        # Increment the date
-        date += datetime.timedelta(days=1)
